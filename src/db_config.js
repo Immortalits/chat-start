d31d1e8d045ccf3438f1f37382aeb27bbb0eacec
ZOLTAN BORBIRO <zazib03@gmail.com>
2021-05-21 18:23:16 +0200

    chat ablak órai rész

---
+++ src/db_config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'AIzaSyBRbUuROVwYMgiLFOnY3d8mOgLE0ZVKazc',
+  authDomain: 'chat-app-immortalits.firebaseapp.com',
+  databaseURL: 'https://chat-app-immortalits.firebaseio.com',
+  projectId: 'chat-app-immortalits',
+  storageBucket: 'chat-app-immortalits.appspot.com',
+  messagingSenderId: '631495085970'
+};
+
+export default config;
